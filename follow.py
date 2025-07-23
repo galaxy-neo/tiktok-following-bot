90fd9820b296be9450266a4b8dd54345555cfeae
neo galaxy <webqart@icloud.com>
2025-07-23 16:39:40 +0300

    Update follow.py

--- follow.py
+++ follow.py
@@ -1,7 +1,7 @@
 # TikTokPrivateApi
 mass registration of tik tok accounts, tik tok likes, comments, tik tok latest version  tiktok API  login to the user by email and username register new device receive information of the user through user_id receive information about your authorized profile get list of user's subscribers get list of user's subscription
 
-### contact me
+# testing api
 **Contact for api :** [https://t.me/Neo_Galaxy](https://t.me/Neo_Galaxy)
 ## telegram: @Neo_Galaxy
 
